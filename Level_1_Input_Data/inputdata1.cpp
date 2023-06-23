535f99937c3a26d5868691cf07cce842617297c1
kadeeja11 <junaid@digiledge.com>
2023-06-23 11:06:21 +0530

    Commit1

--- Level_1_Input_Data/inputdata1.cpp
+++ Level_1_Input_Data/inputdata1.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <fstream>
-#include <json/json.h>
+#include <json.hpp>
 using namespace std;
 int main()
 {
     Json::Value input;
     std::ifstream input_file("input.json", std::ifstream::binary);
     input_file >> input;
-
     cout << input;
 }
